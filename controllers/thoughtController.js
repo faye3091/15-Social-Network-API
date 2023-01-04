be76fc8da5bdb26c101a8843e5505eac7a421898
Vanessa <faye_3091@yahoo.com>
2023-01-04 15:41:22 -0500

    setup controller layout and structure and api routes

---
+++ controllers/thoughtController.js
@@ -0,0 +1,11 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+    getAllThoughts(req,res) {},
+    getThoughtById({ params }, res) {},
+    createThought({ params, body}, res) {},
+    updateThought({ params, body }, res) {},
+    deleteThought({ params }, res) {},
+    addReaction({ params, body}, res) {},
+    deleteReaction({ params }, res) {}
+}
